5dc9ae877014e545af8fba42b09a9a3b927312fe
Wade Huber <wadehuber@gmail.com>
2017-06-20 19:35:11 -0700

    Class 7 changes

--- class7/vectors.cpp
+++ class7/vectors.cpp
@@ -5,37 +5,45 @@ using namespace std;
 
 int main() {
   vector<int> v(10);      // Create vector of size 10
-  int ii;
 
-  v = {34, 23, 30, 19};   // Initialize the vector
+  v = {34, 23, 30, 19};
 
   // display the size of the vector
   cout << "Vector size: " << v.size() << endl;
   cout << "Vector capacity: " << v.capacity() << endl;
 
   // Print vector with a for loop using array syntax (UNSAFE)
-  for(ii=0; ii<v.size(); ii++) {
-    cout << v[ii] << " " ;
+  for (int ii=0; ii < v.capacity(); ii++) {
+    cout << v[ii] << ", ";
   }
   cout << endl;
 
-  cout << "front: " << v.front() << endl;
-  cout << "back: " << v.back() << endl;
+  cout << " front: " << v.front() << endl;
+  cout << " back: " << v.back() << endl;
 
-  v.push_back(42);
-  v.push_back(14);
-  
-  for(ii=0; ii<v.size(); ii++) {
-    cout << v.at(ii) << " " ;
+
+  cout << endl << "push_back 42 & 37" << endl;
+  v.push_back(42);  
+  v.push_back(37);  
+
+  // Print vector using at()
+  //for (int ii=0; ii < v.capacity(); ii++) {
+  for (int ii=0; ii < v.size(); ii++) {
+    cout << v.at(ii) << ", ";
   }
   cout << endl;
-  cout << "back (after push): " << v.back() << endl;
+  cout << " front: " << v.front() << endl;
+  cout << " back: " << v.back() << endl;
 
+
+  cout << endl; 
   v.pop_back();
-  for(ii=0; ii<v.size(); ii++) {
-    cout << v.at(ii) << " " ;
+  cout << "After pop: " << endl;
+  // Print vector using for-each
+  for (auto& ii : v) {
+    cout << ii << ", ";
   }
-   
-  cout << endl;
+  cout << endl; 
+ 
   return 0;
 }
