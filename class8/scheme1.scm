710557382e0584b2a6bc10fd61b4087aaf5c1a67
Wade Huber <wadehuber@gmail.com>
2017-06-22 20:15:10 -0700

    Class 8 updates

--- class8/scheme1.scm
+++ class8/scheme1.scm
@@ -1,28 +1,31 @@
 #lang scheme
-; literals
-"Math"
-3
+"Math"   ;string
+3        ;integer
+'Sym     ;symbol
++        ;primitive
 
-;Math
+; Math
 (* 10 10)
 (+ 1 2)
 (* 3 4)
 (+ 1 2 3 4 5 6 7 8 9 10)
 (* 1 2 3 4 5 6 7 8 9 10)
-(* (/ (* 7 (- 8 2)) (/ (* 6 3) (* 2 2 3 (/ 1 2)))))
+(* (/ (* 7 (- 8 2)) (/ (* 6 3) (* 2 2 3 (/ 1 2)))) 2)
 
 (newline)
 "Division"
+(/ 30 7)
+(/ 30 7.0)
 (remainder 20 8)
-(/ 20 8)
-(/ 20.0 8.0)
+(quotient 20 8)
+(quotient 20 8.0)
 '(/ 10 5)
 
 (newline)
 "Define x & y"
 (define x 10)
 (define y 20)
-(define (z) 30)   ; this defines a scheme form that evaluates to 30
+(define (z) 30)
 x
 y
 z
@@ -30,14 +33,17 @@ z
 
 (newline)
 "Define a procedure"
+(* 3 x)
 (lambda (x) (* 3 x))  ; unnamed procedure
 ((lambda (x) (* 3 x)) 9)
 ((lambda (x) (* 3 x)) y)
-(define trip (lambda (x) (* 3 x)))
+(define trip          ; associates a name to a Scheme form
+  (lambda (x)
+    (* 3 x)))
+trip
 (trip 9)
 (trip y)
-(trip (z))
-(+ x (z) (trip 5))
+(+ x y (trip 5))
 
 (newline)
 "Recursion"
@@ -47,18 +53,21 @@ z
         1
         (* n (fact (- n 1))))))
 (fact 10)
+(fact 30)
+(fact 100)
 ;(fact 1000)
+;(fact 10000)
 
 "tail-recursion"
 (define fact-tail
   (lambda (n)
     (fact-tail-acc n 1)))
-(define fact-tail-acc
+(define fact-tail-acc 
   (lambda (n acc)
     (if (<= n 1)
         acc
         (fact-tail-acc (- n 1) (* n acc)))))
-(fact-tail 10)
+(fact-tail 100)
 
 (newline)
 "lists"
@@ -66,22 +75,27 @@ z
 (list 'x 'y 'z)
 '(x y z)
 (quote (x y z))
+(quote z)
 (define lst '(a b c))
-(define lst2 (list 'a 2 "hello" 'world / + fact-tail 18 'a #\H))
+(define lst2 (list 'a 2 "hello" 'world / #f + fact-tail 18 'a #\H))
 (define oplist (list + / -))
 lst
 lst2
+oplist
+lst
 (car lst)
 (cdr lst)
 (car (cdr lst))
+(cdr (cdr lst))
 (car (cdr (cdr lst)))
 (cdr (cdr (cdr lst)))
-(cdr (list 1))
-(cadr lst)  ; car of the cdr of lst
+(cadr lst)
 ;(car '())
-(caddr lst)
 (car '((4 5 6)))
 (cdr (car '((4 5 6))))
+(define lst3 '((1 2) (3 4 (5 6)) (7 8)))
+lst3
+(cdr '((1 2) (3 4 (5 6)) (7 8)))
 (car (cdr (car (cdr '((1 2) (3 4 (5 6)) (7 8))))))  ; 4
 (cdr (cdr (car (cdr '((1 2) (3 4 (5 6)) (7 8))))))  ; ((5 6))
 (car (cdr (cdr (car (cdr '((1 2) (3 4 (5 6)) (7 8))))))) ; (5 6)
@@ -95,6 +109,7 @@ lst2
 (eq? 32 32)
 (equal? 32 32)
 (equal? 32 34)
+(newline)
 (eq? 'X 'X)
 (eq? '(1 2 3) '(1 2 3))
 (equal? 'X 'X)
@@ -116,6 +131,8 @@ lst2
 oplist
 ((car oplist) 1 2 3)
 ((car (cdr oplist)) 24 7)
+lst
+
 lst2
 (cddddr lst2)
 (cadddr lst2)
